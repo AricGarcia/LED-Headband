4faf9e03e6180d7f27c857254d31116a5d290538
agarcia <agarcia@skybox.com>
2017-12-03 19:38:52 -0800

    Completion of LP5562 Lib

---
+++ dev/flowerband2/flowerband2.ino
@@ -0,0 +1,21 @@
+#include <LP5562.h>
+
+/*
+ * Aric Garcia, 2017-12-03
+ * 
+ * Flower Band 2
+ * Using LP5562 LED driver. Run code for headband.
+*/
+
+// initialize at i2c address 0
+LP5562 lp5562(0);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
